d4d218a459e76d83fdfe3ea7abd799baaafb9f1d
Dave Kuntz <dkuntz811@gmail.com>
2016-09-29 12:32:56 -0500

    items appended to DOM

--- modules/connection.js
+++ modules/connection.js
@@ -1,2 +1,2 @@
-var connection = 'mongodb://localhost:27017/shelfobjects';
-module.exports = connection;
+// var connection = 'mongodb://localhost:27017/shelfobjects';
+// module.exports = connection;
